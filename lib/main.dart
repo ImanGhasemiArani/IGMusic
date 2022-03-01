4a9aa069a34eb1b384776ebea7d3f51cd02bbfca
Iman Ghasemi Arani <87601518+ImanGhasemiArani@users.noreply.github.com>
2022-03-01 04:43:52 +0330

    fixed bugs about load data when change theme

--- lib/main.dart
+++ lib/main.dart
@@ -41,56 +41,65 @@ class MainMaterial extends StatelessWidget {
 
     return Builder(builder: (context) {
       return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: "IGMusic",
-          //
-          //
-          //
-          themeMode: Provider.of<ThemeNotifier>(context).getTheme(),
-          //
-          theme: ThemeData(
-            bottomSheetTheme:
-                const BottomSheetThemeData(backgroundColor: Colors.transparent),
-            brightness: Brightness.light,
-            iconTheme: const IconThemeData(color: Colors.black),
-            textTheme:
-                const TextTheme(bodyText2: TextStyle(color: Colors.black)),
-            colorScheme: const ColorScheme.light().copyWith(
-              secondary: Colors.amber,
-              onSecondary: Colors.black,
-              tertiary: Colors.teal,
-              onTertiary: Colors.black,
-            ),
+        debugShowCheckedModeBanner: false,
+        title: "IGMusic",
+        //
+        //
+        //
+        themeMode: Provider.of<ThemeNotifier>(context).getTheme(),
+        //
+        theme: ThemeData(
+          bottomSheetTheme:
+              const BottomSheetThemeData(backgroundColor: Colors.transparent),
+          brightness: Brightness.light,
+          iconTheme: const IconThemeData(color: Colors.black),
+          textTheme: const TextTheme(bodyText2: TextStyle(color: Colors.black)),
+          colorScheme: const ColorScheme.light().copyWith(
+            secondary: Colors.amber,
+            onSecondary: Colors.black,
+            tertiary: Colors.teal,
+            onTertiary: Colors.black,
           ),
-          //
-          darkTheme: ThemeData(
-            bottomSheetTheme:
-                const BottomSheetThemeData(backgroundColor: Colors.transparent),
-            brightness: Brightness.dark,
-            iconTheme: const IconThemeData(color: Color(0xFFBDBDBD)),
-            textTheme:
-                const TextTheme(bodyText2: TextStyle(color: Color(0xFFBDBDBD))),
-            colorScheme: const ColorScheme.dark().copyWith(
-              secondary: Colors.amber,
-              onSecondary: Colors.black,
-              tertiary: Colors.teal,
-              onTertiary: Colors.black,
-            ),
+        ),
+        //
+        darkTheme: ThemeData(
+          bottomSheetTheme:
+              const BottomSheetThemeData(backgroundColor: Colors.transparent),
+          brightness: Brightness.dark,
+          iconTheme: const IconThemeData(color: Color(0xFFBDBDBD)),
+          textTheme:
+              const TextTheme(bodyText2: TextStyle(color: Color(0xFFBDBDBD))),
+          colorScheme: const ColorScheme.dark().copyWith(
+            secondary: Colors.amber,
+            onSecondary: Colors.black,
+            tertiary: Colors.teal,
+            onTertiary: Colors.black,
           ),
-          //
-          //
-          //
-          home: FutureBuilder<bool>(
-            builder: (context, snapshot) {
-              if (snapshot.hasData && snapshot.data as bool) {
-                FlutterNativeSplash.remove();
-                return const ScreenHolder();
-              }
-              return const SplashScreen();
-            },
-            future: preLoadDat(),
-          ));
+        ),
+        //
+        //
+        //
+        home: const ScreenApp(),
+      );
     });
+  }
+}
+
+class ScreenApp extends StatelessWidget {
+  const ScreenApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<bool>(
+      builder: (context, snapshot) {
+        if (snapshot.hasData && snapshot.data as bool) {
+          FlutterNativeSplash.remove();
+          return const ScreenHolder();
+        }
+        return const SplashScreen();
+      },
+      future: preLoadDat(),
+    );
   }
 
   Future<bool> preLoadDat() async {
@@ -98,7 +107,7 @@ class MainMaterial extends StatelessWidget {
       if (value) {
         await fastLoadUserData();
         //   await checkStorage();
-        // Future.delayed(const Duration(seconds: 5), checkStorage);
+        Future.delayed(const Duration(seconds: 5), checkStorage);
       }
     });
     return true;
