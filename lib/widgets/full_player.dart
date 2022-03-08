f3243665d5c4c3044ec4dbde7c751fc983e0a7a9
Iman Ghasemi Arani <87601518+ImanGhasemiArani@users.noreply.github.com>
2022-03-08 05:08:33 +0330

    create curved text

--- lib/widgets/full_player.dart
+++ lib/widgets/full_player.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:glassmorphism_ui/glassmorphism_ui.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:sleek_circular_slider/sleek_circular_slider.dart';
+import 'package:flutter_arc_text/flutter_arc_text.dart';
 
 import '../assets/fnt_styles.dart';
 import '../assets/icos.dart';
@@ -16,6 +19,7 @@ import 'button/btn_favourite.dart';
 import 'button/btn_play_pause.dart';
 import 'button/btn_skip_next.dart';
 import 'button/btn_skip_previous.dart';
+import 'button/tap_effect.dart';
 
 class FullPlayer extends StatelessWidget {
   const FullPlayer({Key? key, required this.closeButtonOnTap})
@@ -64,10 +68,26 @@ class FullPlayer extends StatelessWidget {
             ),
           ),
           Align(
+            alignment: Alignment.topRight,
+            child: Padding(
+              padding: EdgeInsets.only(
+                  right: size.width * 0.1 - 27, top: size.width * 0.1),
+              child: TapEffect(
+                padding: const EdgeInsets.all(17),
+                onTap: closeButtonOnTap,
+                child: const Icon(
+                  Icos.dots,
+                  size: 20,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Align(
             alignment: Alignment.topCenter,
-            child: ValueListenableBuilder<SongMetadata>(
-                valueListenable: currentSongMetaDataNotifier,
-                builder: (_, metadata, __) {
+            child: ValueListenableBuilder<Uint8List?>(
+                valueListenable: currentSongArtworkNotifier,
+                builder: (_, artwork, __) {
                   return Container(
                     height: size.height * 0.5,
                     width: size.width * 0.6,
@@ -75,7 +95,7 @@ class FullPlayer extends StatelessWidget {
                       borderRadius: const BorderRadius.vertical(
                           bottom: Radius.circular(1000)),
                       image: DecorationImage(
-                        image: getArtwork(artworkData: metadata.artwork).image,
+                        image: getArtwork(artworkData: artwork).image,
                         fit: BoxFit.cover,
                       ),
                       boxShadow: const [
@@ -84,50 +104,6 @@ class FullPlayer extends StatelessWidget {
                           blurRadius: 10,
                         ),
                       ],
-                    ),
-                    child: Align(
-                      alignment: Alignment.bottomCenter,
-                      child: FittedBox(
-                        fit: BoxFit.scaleDown,
-                        child: Padding(
-                          padding:
-                              EdgeInsets.fromLTRB(0, 0, 0, size.height * 0.1),
-                          child: GlassContainer(
-                            width: size.width * 0.5,
-                            blur: 8,
-                            opacity: 0.08,
-                            border:
-                                const Border.fromBorderSide(BorderSide.none),
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(100)),
-                            child: Padding(
-                              padding: const EdgeInsets.all(10),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  Text(
-                                    metadata.title,
-                                    style: FntStyles
-                                        .songFullItemWidgetTrackNameStyle,
-                                    overflow: TextOverflow.ellipsis,
-                                    textAlign: TextAlign.center,
-                                  ),
-                                  Text(
-                                    metadata.artist,
-                                    style: FntStyles
-                                        .songFullItemWidgetArtistNameStyle,
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
-                                    textAlign: TextAlign.center,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
                     ),
                   );
                 }),
@@ -166,7 +142,7 @@ class FullPlayer extends StatelessWidget {
                       alignment: Alignment.topLeft,
                       child: Padding(
                         padding: EdgeInsets.only(
-                          top: size.width * 0.6 - 20,
+                          top: size.width * 0.6 - 50,
                           left: size.width * 0.1,
                         ),
                         child: Text(
@@ -179,7 +155,7 @@ class FullPlayer extends StatelessWidget {
                       alignment: Alignment.topRight,
                       child: Padding(
                         padding: EdgeInsets.only(
-                          top: size.width * 0.6 - 20,
+                          top: size.width * 0.6 - 50,
                           right: size.width * 0.1,
                         ),
                         child: Text(
@@ -192,6 +168,66 @@ class FullPlayer extends StatelessWidget {
                 );
               },
             ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: ValueListenableBuilder<SongMetadata>(
+                valueListenable: currentSongMetaDataNotifier,
+                builder: (_, metadata, __) {
+                  return FittedBox(
+                    alignment: Alignment.topCenter,
+                    fit: BoxFit.scaleDown,
+                    child: SizedBox(
+                      height: size.height * 0.5,
+                      width: size.width * 0.7,
+                      child: Column(
+                        children: [
+                          Align(
+                            alignment: Alignment.topCenter,
+                            child: Padding(
+                              padding: EdgeInsets.only(
+                                top: size.height * 0.5 - size.width * 0.3 + 20,
+                              ),
+                              child: ArcText(
+                                text: metadata.title,
+                                textStyle: GoogleFonts.rajdhani(
+                                  fontSize: 15,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w900,
+                                ),
+                                radius: size.width * 0.3 + 20,
+                                placement: Placement.outside,
+                                startAngle: 180 / 180 * pi,
+                                startAngleAlignment: StartAngleAlignment.center,
+                                direction: Direction.counterClockwise,
+                              ),
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.topCenter,
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                top: 30,
+                              ),
+                              child: ArcText(
+                                text: metadata.artist,
+                                textStyle: GoogleFonts.itim(
+                                  fontSize: 11,
+                                  color: Colors.white,
+                                ),
+                                radius: size.width * 0.3 + 20,
+                                placement: Placement.outside,
+                                startAngle: 180 / 180 * pi,
+                                startAngleAlignment: StartAngleAlignment.center,
+                                direction: Direction.counterClockwise,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }),
           ),
           Align(
             alignment: Alignment.topCenter,
