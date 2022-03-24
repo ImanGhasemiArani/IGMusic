9c76c4032f1e129bfcf1ed17bc5f8afc066f56b6
Iman Ghasemi Arani <87601518+ImanGhasemiArani@users.noreply.github.com>
2022-03-24 13:11:46 +0430

    refactor mini player and add like btn

--- lib/widgets/mini_player.dart
+++ lib/widgets/mini_player.dart
@@ -4,10 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:glassmorphism_ui/glassmorphism_ui.dart';
 
 import '../assets/fonts.dart';
+import '../assets/imgs.dart';
 import '../controllers/value_notifier.dart';
 import '../models/song_metadata.dart';
 import '../util/audio_info.dart';
 import '../util/util_artwork.dart';
+import 'button/btn_favorite.dart';
 import 'button/btn_play_pause.dart';
 import 'button/btn_skip_next.dart';
 import 'button/btn_skip_previous.dart';
@@ -19,98 +21,118 @@ class MiniPlayer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder<Uint8List?>(
-      valueListenable: currentSongArtworkNotifier,
-      builder: (_, value, __) {
-        return Stack(children: [
-          Container(
-            height: double.infinity,
-            width: double.infinity,
+    return Stack(
+      children: [
+        ValueListenableBuilder<Uint8List?>(
+          valueListenable: currentSongArtworkNotifier,
+          builder: (_, artwork, __) {
+            return Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(Imgs.imgDefaultMusicCover),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: getArtwork(artworkData: artwork),
+            );
+          },
+        ),
+        Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            height: 4,
+            width: maxWidth * 0.12,
+            margin: const EdgeInsets.only(top: 15),
             decoration: BoxDecoration(
-              image: DecorationImage(
-                image: getArtwork(artworkData: value).image,
-                fit: BoxFit.cover,
+              color: Colors.white.withOpacity(0.8),
+              borderRadius: BorderRadius.circular(100),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(1),
+                  blurRadius: 8,
+                ),
+              ],
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.topRight,
+          child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: GlassContainer(
+              blur: 10,
+              opacity: 0.2,
+              borderRadius: const BorderRadius.all(Radius.circular(100)),
+              child: ValueListenableBuilder<SongMetadata>(
+                valueListenable: currentSongMetaDataNotifier,
+                builder: (_, metadata, __) {
+                  return BtnFavorite(
+                    songMetadata: metadata,
+                    size: 25,
+                  );
+                },
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: Align(
-              alignment: Alignment.topCenter,
-              child: Container(
-                  height: 4,
-                  width: maxWidth * 0.12,
-                  decoration: BoxDecoration(
-                      color: Colors.white.withOpacity(0.8),
-                      borderRadius: BorderRadius.circular(100),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(1),
-                          blurRadius: 8,
-                        ),
-                      ])),
-            ),
-          ),
-          Padding(
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
             padding: const EdgeInsets.only(bottom: 30),
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: FittedBox(
-                fit: BoxFit.scaleDown,
-                alignment: Alignment.center,
-                child: GlassContainer(
-                  width: maxWidth / 3 * 2,
-                  //   height: 120,
-                  blur: 50,
-                  border: const Border.fromBorderSide(BorderSide.none),
-                  opacity: 0.05,
-                  borderRadius: BorderRadius.circular(20),
-                  child: Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: ValueListenableBuilder<SongMetadata>(
-                        valueListenable: currentSongMetaDataNotifier,
-                        builder: (_, metadata, __) {
-                          var infoList = exportData(
-                              metadata.title, metadata.artist, metadata.album);
-                          var title = infoList[0];
-                          var artist = infoList[1];
-                          return Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text(
-                                  title,
-                                  textAlign: TextAlign.center,
-                                  style: Fonts.rajdhani_16_w900_ffffffff,
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                                Text(
-                                  artist,
-                                  textAlign: TextAlign.center,
-                                  style: Fonts.overlock_15_ffffffff,
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: const [
-                                    BtnSkipPrevious(),
-                                    BtnPlayPause(),
-                                    BtnSkipNext(),
-                                  ],
-                                ),
-                              ]);
-                        }),
-                  ),
+            child: FittedBox(
+              fit: BoxFit.scaleDown,
+              alignment: Alignment.center,
+              child: GlassContainer(
+                width: maxWidth / 3 * 2,
+                blur: 10,
+                borderRadius: BorderRadius.circular(20),
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: ValueListenableBuilder<SongMetadata>(
+                      valueListenable: currentSongMetaDataNotifier,
+                      builder: (_, metadata, __) {
+                        var infoList = exportData(
+                            metadata.title, metadata.artist, metadata.album);
+                        var title = infoList[0];
+                        var artist = infoList[1];
+                        return Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                title,
+                                textAlign: TextAlign.center,
+                                style: Fonts.rajdhani_16_w900_ffffffff,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              Text(
+                                artist,
+                                textAlign: TextAlign.center,
+                                style: Fonts.overlock_15_ffffffff,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: const [
+                                  BtnSkipPrevious(),
+                                  BtnPlayPause(),
+                                  BtnSkipNext(),
+                                ],
+                              ),
+                            ]);
+                      }),
                 ),
               ),
             ),
-          )
-        ]);
-      },
+          ),
+        ),
+      ],
     );
   }
 }
