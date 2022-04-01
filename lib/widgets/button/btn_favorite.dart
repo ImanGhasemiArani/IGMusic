a1fe9622abf2384da0d9978847e4c17dcfac7dcf
Iman Ghasemi Arani <87601518+ImanGhasemiArani@users.noreply.github.com>
2022-04-01 14:55:31 +0430

    refactor fav btn with getx

--- lib/widgets/button/btn_favorite.dart
+++ lib/widgets/button/btn_favorite.dart
@@ -1,36 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../../assets/icos.dart';
 import '../../controllers/btn_controllers.dart';
 import '../../models/song_metadata.dart';
 import 'tap_effect.dart';
 
-// ignore: must_be_immutable
 class BtnFavorite extends StatelessWidget {
-  BtnFavorite({Key? key, this.size = 20, required this.songMetadata})
-      : isLiked = songMetadata.isLiked,
-        super(key: key);
+  const BtnFavorite({Key? key, this.size = 20, required this.songMetadata})
+      : super(key: key);
 
-  bool isLiked;
   final SongMetadata songMetadata;
   final double size;
 
   @override
   Widget build(BuildContext context) {
-    return StatefulBuilder(builder: (context, setState) {
-      return TapEffect(
-        onTap: () {
-          setState(() {
-            isLiked = !isLiked;
-          });
-          btnLikeTaped(songMetadata: songMetadata, isLiked: isLiked);
-        },
-        child: Icon(
-          isLiked ? Icos.heart : Icos.heart_outlined,
-          color: isLiked ? Colors.red : Colors.white.withOpacity(0.7),
+    final isLiked = songMetadata.isLiked.obs;
+    return TapEffect(
+      onTap: () {
+        isLiked.value = !isLiked.value;
+        btnLikeTaped(songMetadata: songMetadata, isLiked: isLiked.value);
+      },
+      child: Obx(
+        () => Icon(
+          isLiked.value ? Icos.heart : Icos.heart_outlined,
+          color: isLiked.value ? Colors.red : Colors.white.withOpacity(0.7),
           size: size,
         ),
-      );
-    });
+      ),
+    );
   }
 }
